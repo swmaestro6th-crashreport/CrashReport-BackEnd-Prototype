752b84c30ff99bc40cfc619071bf515dad8214b7
Dainel Park <andmylife@naver.com>
2015-08-28 06:55:42 +0900

    inserting id error fixed

--- routes/projects.js
+++ routes/projects.js
@@ -7,7 +7,7 @@ router.post('/', function(req, res) {
     if (!req.get('user_id')) res.json({ code : 101, message : "failed" });
     else {
       new Project({
-        id: req.get('user_id'),
+        userId: req.get('user_id'),
         name: req.body.name,
         appVersion: req.body.appversion,
         apiKey: crypto.randomBytes(10).toString('hex')
@@ -20,14 +20,16 @@ router.post('/', function(req, res) {
 
 router.get('/:user_id', function (req, res) {
     Project.find({ "userId" : req.params.user_id }, function (err, projects) {
-        
-        var projectIds = {};
+      
+        console.log(projects.length);
+
+        var projectIds = [];
 
         projects.forEach(function (projects) {
-            projectIds['userid'] = projects.id;
-        });
-
-        res.json({ code: 200, projects: projectIds });
+            projectIds.push(projects.id);
+        }); 
+        
+        res.json({ code: 200, projects: projects });
     })
 });
 
